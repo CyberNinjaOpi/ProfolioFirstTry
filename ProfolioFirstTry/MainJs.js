f4c41b7557327ef9ff21939a4e552bdf867604f2
opielanaruto <opielanaruto@gmail.com>
2014-07-30 15:59:21 -0500

    Portfolio initial

---
+++ ProfolioFirstTry/MainJs.js
@@ -0,0 +1,14 @@
+﻿var app = angular.module("app", ["ngRoute"])
+app.config(function ($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "Views/home.html",
+        controller: "HomeController"
+    })
+    .when("/contact", {
+        templateUrl: "Views/contact.html",
+        controller: "ContactsController"
+    })
+    .otherwise({
+        templateUrl: "Views/error.html"
+    })
+});
