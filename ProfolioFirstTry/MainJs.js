cfe71d2758fa959b024c2048995a789a8ff6cb92
opielanaruto <opielanaruto@gmail.com>
2014-08-22 14:32:03 -0700

    Finished

--- ProfolioFirstTry/MainJs.js
+++ ProfolioFirstTry/MainJs.js
@@ -1,14 +1,38 @@
 ﻿var app = angular.module("app", ["ngRoute"])
 app.config(function ($routeProvider) {
-    $routeProvider.when("/", {
-        templateUrl: "Views/home.html",
-        controller: "HomeController"
-    })
+	$routeProvider.when("/", {
+		templateUrl: "Views/home.html",
+		controller: "HomeController"
+	})
+	.when("/home2", {
+		templateUrl: "Views/home2.html",
+		controller: "Home2Controller"
+	})
+	.when("/todoapp", {
+		templateUrl: "Views/todoapp.html",
+		controller: "ToDoController"
+	})
+	.when("/quizapp", {
+		templateUrl: "Views/quizapp.html",
+		controller: "QuizController"
+	})
+	.when("/spaceapp", {
+		templateUrl: "Views/spaceapp.html",
+		controller: "SpaceShipController"
+	})
+	.when("/spaceport", {
+		templateUrl: "Views/spaceport.html",
+		controller: "SpacePortController"
+	})
+	.when("/chirper", {
+		templateUrl: "Views/chirper.html",
+		controller: "ChirperController"
+	})
     .when("/contact", {
-        templateUrl: "Views/contact.html",
-        controller: "ContactsController"
+    	templateUrl: "Views/contact.html",
+    	controller: "ContactsController"
     })
     .otherwise({
-        templateUrl: "Views/error.html"
+    	templateUrl: "Views/error.html"
     })
 });
